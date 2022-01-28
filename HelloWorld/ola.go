b172b8157dfb442654f94abb2a2940b0950a753d
Mateus Rotta <roteus@gmail.com>
2022-01-28 17:36:06 -0300

    Refactoring the hello world

--- HelloWorld/ola.go
+++ HelloWorld/ola.go
@@ -2,8 +2,13 @@ package main
 
 import "fmt"
 
+const prefixoOlaPortugues = "Olá, "
+
 func Ola(nome string) string {
-	return "Olá, " + nome
+	if nome == "" {
+		nome = "mundo"
+	}
+	return prefixoOlaPortugues + nome
 }
 
 func main() {
--- HelloWorld/ola_test.go
+++ HelloWorld/ola_test.go
@@ -3,10 +3,24 @@ package main
 import "testing"
 
 func TestOla(t *testing.T) {
-	resultado := Ola("Chris")
-	esperado := "Olá, Chris"
-
-	if resultado != esperado {
-		t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+	verificaMensagemCorreta := func(t *testing.T, resultado, esperado string) {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado '%s', esperado '%s'", resultado, esperado)
+		}
 	}
+
+	t.Run("diz olá para as pessoas", func(t *testing.T) {
+		resultado := Ola("Chris")
+		esperado := "Olá, Chris"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
+		resultado := Ola("")
+		esperado := "Olá, mundo"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
 }
