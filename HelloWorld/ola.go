c16555949eb0d0f3fc3ca9eaa7cccff5a932b93a
Mateus Rotta <roteus@gmail.com>
2022-01-31 15:30:08 -0300

    Refactoring and adding functions to make our hello world more readable

--- HelloWorld/ola.go
+++ HelloWorld/ola.go
@@ -2,15 +2,35 @@ package main
 
 import "fmt"
 
+const espanhol = "espanhol"
+const frances = "frances"
+const esperanto = "esperanto"
+const prefixoOlaEsperanto = "Saluton, "
 const prefixoOlaPortugues = "Olá, "
+const prefixoOlaEspanhol = "Hola, "
+const prefixoOlaFrances = "Bonjour, "
 
-func Ola(nome string) string {
+func Ola(nome, idioma string) string {
 	if nome == "" {
 		nome = "mundo"
 	}
-	return prefixoOlaPortugues + nome
+	return prefixoSaudacao(idioma) + nome
+}
+
+func prefixoSaudacao(idioma string) (prefixo string) {
+	switch idioma {
+	case frances:
+		prefixo = prefixoOlaFrances
+	case espanhol:
+		prefixo = prefixoOlaEspanhol
+	case esperanto:
+		prefixo = prefixoOlaEsperanto
+	default:
+		prefixo = prefixoOlaPortugues
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Ola("mundo"))
+	fmt.Println(Ola("mundo", prefixoOlaPortugues))
 }
--- HelloWorld/ola_test.go
+++ HelloWorld/ola_test.go
@@ -11,15 +11,36 @@ func TestOla(t *testing.T) {
 	}
 
 	t.Run("diz olá para as pessoas", func(t *testing.T) {
-		resultado := Ola("Chris")
+		resultado := Ola("Chris", "")
 		esperado := "Olá, Chris"
 
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
 
 	t.Run("diz 'Olá, mundo' quando uma string vazia for passada", func(t *testing.T) {
-		resultado := Ola("")
+		resultado := Ola("", "")
 		esperado := "Olá, mundo"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em espanhol", func(t *testing.T){
+		resultado := Ola("Chris", "espanhol")
+		esperado := "Hola, Chris"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em francês", func(t *testing.T){
+		resultado := Ola("Chris", "frances")
+		esperado := "Bonjour, Chris"
+
+		verificaMensagemCorreta(t, resultado, esperado)
+	})
+
+	t.Run("em esperanto", func(t *testing.T){
+		resultado := Ola("Chris", "esperanto")
+		esperado := "Saluton, Chris"
 
 		verificaMensagemCorreta(t, resultado, esperado)
 	})
